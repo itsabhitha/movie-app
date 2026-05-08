238a8b2f789d2c08128f987ca1cf02c1bdeb1a71
Your Name <your_email@example.com>
2026-05-08 23:40:51 +0530

    Fixed favorites page issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import "./css/App.css";
-import Favourites from "./pages/Favourites";
-import Favorite from "./pages/Favourites";
-import Home from "./pages/Home"
-import {Routes,Route} from "react-router-dom"
+import Favorites from "./pages/Favourites.jsx";
+import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar/>
-    <main className="main-content">
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/favourites" element={<Favourites />}/>
-    </Routes>
-    </main>
-    </div>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
-export default App
+export default App;
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,10 +1,29 @@
-import "../css/Favourites.css"
+import "../css/Favourites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
-function Favourites(){
-    return <div className="favorites-empty">
-        <h2>No favourite Movies Yet</h2>
-        <p> Start adding movies to favourites and they will apppear here</p>
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
+  );
 }
 
-export default Favourites
+export default Favorites;
