0767e8804331ac2e6e1609b3142f5568e5311fc9
Your Name <your_email@example.com>
2026-05-08 21:28:51 +0530

    Initial commit

---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favourites.css"
+
+function Favourites(){
+    return <div className="favorites-empty">
+        <h2>No favourite Movies Yet</h2>
+        <p> Start adding movies to favourites and they will apppear here</p>
+    </div>
+}
+
+export default Favourites
